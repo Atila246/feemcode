ff2f8de1630ad315f53ae11b3a20c6db150909f5
Atila <thalitinha246@gmail.com>
2024-11-27 11:09:33 -0300

    arrumando bugs do perfil do usuário, e adicionando as postagens

--- web/public/js/perfil-usuario.js
+++ web/public/js/perfil-usuario.js
@@ -32,8 +32,6 @@
 //     }      
 // }
 
-
-
 const perfil_nome = document.getElementById("perfil-nome")
 const underline = document.querySelector('.underline')
 const aba1 = document.getElementById("aba1")
@@ -41,6 +39,8 @@ const aba2 = document.getElementById("aba2")
 
 const conteudos_perfil = document.querySelector(".conteudos-perfil")
 const comunidades_perfil = document.querySelector(".comunidades-perfil")
+
+const gerenciar_perfil = document.querySelector(".gerenciar-perfil")
 
 
 aba1.addEventListener('click', () => {
@@ -53,14 +53,13 @@ aba1.addEventListener('click', () => {
     let tamanhoAba2 = underline.style.width = `${aba2.offsetWidth}px`
     let tamanhoAba1 = underline.style.width = `${aba1.offsetWidth}px`
     underline.style.width = tamanhoAba1
-    console.log(tamanhoAba1)
+  
     document.documentElement.style.setProperty('--tamanho-aba-2',tamanhoAba2)
     
     underline.classList.add('animateout')
 
-    conteudos_perfil.style.cssText = "display: block;"
-    comunidades_perfil.style.cssText = "display: none;"
-
+    conteudos_perfil.style.display = "block"
+    comunidades_perfil.style.display = "none"
 })
 
 aba2.addEventListener('click', () => {
@@ -73,13 +72,13 @@ aba2.addEventListener('click', () => {
     let tamanhoAba1 = underline.style.width = `${aba1.offsetWidth}px`
     let tamanhoAba2 = underline.style.width = `${aba2.offsetWidth}px`
     underline.style.width = tamanhoAba2
-    // console.log(tamanhoAba1)
     document.documentElement.style.setProperty('--tamanho-aba-1',tamanhoAba1)
 
     underline.classList.add('animatein')
 
-    conteudos_perfil.style.cssText = "display: none;"
-    comunidades_perfil.style.cssText = "display: flex;"
+    conteudos_perfil.style.display = "none"
+    comunidades_perfil.style.display = "flex"
+    
 })
 
 
