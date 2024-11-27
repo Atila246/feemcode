d747e4addb30022c39505e695a6cb540d762d7eb
Ana Clara <anaclarafontinelescosta@gmail.com>
2024-11-27 14:27:13 -0300

    fazendo o formulário de publicação e edição do usuário, e arrumando a comunidade

--- web/public/js/perfil-comunidade.js
+++ web/public/js/perfil-comunidade.js
@@ -1,47 +1,24 @@
-const perfil_nome = document.getElementById("perfil-nome")
-const underline = document.querySelector('.underline')
-const aba1 = document.getElementById("aba1")
-const aba2 = document.getElementById("aba2")
+/*modal*/
+var publicar_btn = document.getElementById('publicar-btn')
+var publicacao_modal = document.getElementById('publicacao-modal'); 
+var close_btn = document.getElementsByClassName('close');
 
-const conteudos_perfil = document.querySelector(".conteudos-perfil")
-const comunidades_perfil = document.querySelector(".comunidades-perfil")
+publicar_btn.onclick = function () {
+    publicacao_modal.style.display = "flex"
+    console.log(publicacao_modal)
+}
 
-const gerenciar_perfil = document.querySelector(".gerenciar-perfil")
+// Fechar POPUP e formulário ao clicar no 'X'
+for (let i = 0; i < close_btn.length; i++) {
+    close_btn[i].onclick = function () {
+        publicacao_modal.style.display = "none"
+    }
+}
+
+window.onclick = function (event) {
+    if(event.target == publicacao_modal){
+        publicacao_modal.style.display = "none"
+    }      
+}
 
 
-aba1.addEventListener('click', () => {
-    aba1.classList.add('ativo')
-    aba2.classList.remove('ativo')
-    
-    underline.classList.remove('animatein')
-    underline.classList.remove('animateout')
-
-    let tamanhoAba2 = underline.style.width = `${aba2.offsetWidth}px`
-    let tamanhoAba1 = underline.style.width = `${aba1.offsetWidth}px`
-    underline.style.width = tamanhoAba1
-  
-    document.documentElement.style.setProperty('--tamanho-aba-2',tamanhoAba2)
-    
-    underline.classList.add('animateout')
-    
-    gerenciar_perfil.style.display = "none"
-    conteudos_perfil.style.display = "block"    
-})
-
-aba2.addEventListener('click', () => {
-    aba2.classList.add('ativo')
-    aba1.classList.remove('ativo')
-    
-    underline.classList.remove('animateout')
-    underline.classList.remove('animatein')
-    
-    let tamanhoAba1 = underline.style.width = `${aba1.offsetWidth}px`
-    let tamanhoAba2 = underline.style.width = `${aba2.offsetWidth}px`
-    underline.style.width = tamanhoAba2
-    document.documentElement.style.setProperty('--tamanho-aba-1',tamanhoAba1)
-
-    underline.classList.add('animatein')
-
-    gerenciar_perfil.style.display = "flex"
-    conteudos_perfil.style.display = "none" 
-})
